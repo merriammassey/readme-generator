c0fc4a8695dce47e0f1dc6b9135774b11ef9c260
Maria M <prov21_1@hotmail.com>
2021-03-28 12:55:09 -0700

    mockdata

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -9,5 +10,38 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {}
 
+/*const promptUser = ()=> {
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'project title',
+        message: 'What is the project title? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            }else {
+                console.log('Please enter your project title.');
+                return false;
+            }
+        }
+    },
+    
+    
+
+])
+} */
+
 // Function call to initialize app
 init();
+
+//mock data
+const mockData = {
+    title: 'My Title',
+    description: 'Lorem ipsum',
+    installationInstructions: 'to install, you must',
+    usageInfo: 'use this for...',
+    contributionGuidelines: 'to contribute...',
+    testInstructions: 'to test...'
+  }
+  
+  const pageHTML = generatePage(mockData);
